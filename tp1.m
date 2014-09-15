cba87c7943526a3c3f491f766515fe955b7f12cb
Sabrina <sizcovich@dc.uba.ar>
2014-09-15 17:15:59 -0300

    subo todo

--- tp1.m
+++ tp1.m
@@ -16,53 +16,69 @@ vbl = Screen('Flip', win);
 %Screen('MakeTexture', win, myImage);
 %Screen('DrawTexture', win, textureIndex);
 
-% imagenesSubliminales = []
-% imagenesSecundarias = [[]]
+
+imagenesSubliminales = [];
+imagenesSecundarias = [[]];
+imagenesSubliminalesFruta = [];
+imagenesSecundariasFruta = [[]];
+
+wait_pl1 = 2;  % Tiempo que dura la primer pantalla para limpiar retina
+wait_is = 0.0050;   % Tiempo que se muestra la imagen subliminal
+wait_pl2 = 2;  % Tiempo que dura la segunda pantalla para limpiar retina
+
 
 % Aviso de warning de mac
-% Pantalla negra con la descripcion de la tarea
+% Pantalla negra con la descripcion de la tarea:
 % "A continuacion apareceran grupos de 3 imagenes. La tarea consiste en
 % escribir una palabra que se relacione con ellas. 
-%Oprimir la barra espaciadora antes de escribir la palabra"
+% Debera oprimir la barra espaciadora recien cuando este listo para escribir la palabra.
+% Presione una tecla para continuar."
 % wait_init
-% 
+Screen('FillRect',win,black);
+% TODO: mostrar descripcion
+
+% Bucle i = 1:size(imagenesSubliminales)
+% Preguntar: pantalla para limpiar retina
+% wait_pl1
+% Mostrar imagen subliminal
+% wait_is
+% Pantalla limpia-retina
+% wait_pl2
+% Imagenes secundarias
+% Comenzar a medir tiempo
+% wait barra espaciadora
+% Input de texto para escribir
+% wait enter
+% Finalizar medicion de tiempo, guardar tiempo y respuesta
 
 
 semaforo = 0;
 tic
 for i=1:209
+    
+    Screen('FillRect', win, white); % Limpia retina
+    WaitSecs(wait_p1);
+    
+    % TODO: Mostrar imagen suubliminal
+    WaitSecs(wait_is);
+    
+    Screen('FillRect', win, white); % Limpia retina
+    WaitSecs(wait_p2);
+    
+    % TODO: Mostrar imagenes secundarias
+    
+    
     %Draw i?th image to backbuffer:
     %Screen('DrawTexture', win, textureIndex);
     %Show images exactly 2 refresh cycles apart of each other:
     [down, secs, keycode]=KbCheck;
-    if down==1
-        if semaforo==0
-            teclaApretada = [teclaApretada; secs];
-            semaforo = 1;
-        end
-    else
-        semaforo=0;
-    end
-    
-    if mod(i,10)~=0
-        Screen('FillRect',win,black);
-    else
-        if size(teclaApretada,1) < ceil(i/10)
-            teclaApretada = [teclaApretada; 10000];
-        end
-        apariciones = [apariciones; GetSecs];
-        Screen('FillOval', win, white, [500 350 650 450]);
-    end
-    
-    vbl = Screen('Flip', win, vbl + (2-0.1) * refresh);
+    Screen('FillRect', win, black);
     WaitSecs(0.005);
     %Keyboard checks , whatever ... Next loop iteration .
 end;
 %End of animation loop , blank screen , record offset time :
-toffset = Screen('Flip', win, vbl + (2 - 0.5) * refresh);
+%toffset = Screen('Flip', win, vbl + (2 - 0.5) * refresh);
 toc
+
 Screen('CloseAll');
 ShowCursor;
-apariciones
-teclaApretada
-resultadoFallo = teclaApretada(1:20) - apariciones
