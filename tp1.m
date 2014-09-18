8f4a7b655b526a7f760427c859450bdf8b7e8f3e
ramaroberto <bertoski@gmail.com>
2014-09-17 22:46:44 -0300

    Agregado mostrar imagenes y esperar tecla.
    Se agregaron la carga de imagenes tanto para la imagen subliminal como para las imagenes secundarias. Las imagenes secundarias fueron agrupadas en una sola "pegandolas" de izquierda a derecha dejando un margen configurable como separacion; ademas son centradas automaticamente usando como referencia la imagen mas grande. Por el momento solo funciona con la imagen "test.png" pero se puede cambiar facilmente a los vectores usando las direcciones a las imagenes correspondientes. Habria que hacer algo para recorrer los directorios y de alguna forma armar todo programaticamente, asi no tenemos la necesidad de escribir los path a mano.

--- tp1.m
+++ tp1.m
@@ -1,5 +1,5 @@
 screenNum =0;
-res =[1281 800];
+res =[1024 768];
 teclaApretada = [];
 apariciones = [];
 clrdepth=32;
@@ -23,8 +23,10 @@ imagenesSubliminalesFruta = [];
 imagenesSecundariasFruta = [[]];
 
 wait_pl1 = 2;  % Tiempo que dura la primer pantalla para limpiar retina
-wait_is = 0.0050;   % Tiempo que se muestra la imagen subliminal
+wait_is = 0.50;   % Tiempo que se muestra la imagen subliminal
 wait_pl2 = 2;  % Tiempo que dura la segunda pantalla para limpiar retina
+
+imgsec_space = 20; % Tamanio en pixels para los espacios entre las imagenes secundarias
 
 
 % Aviso de warning de mac
@@ -35,6 +37,7 @@ wait_pl2 = 2;  % Tiempo que dura la segunda pantalla para limpiar retina
 % Presione una tecla para continuar."
 % wait_init
 Screen('FillRect',win,black);
+Screen('Flip', win);
 % TODO: mostrar descripcion
 
 % Bucle i = 1:size(imagenesSubliminales)
@@ -54,25 +57,70 @@ Screen('FillRect',win,black);
 
 semaforo = 0;
 tic
-for i=1:209
+for i=1:1
     
     Screen('FillRect', win, white); % Limpia retina
-    WaitSecs(wait_p1);
+    Screen('Flip', win);
+    WaitSecs(wait_pl1);
     
-    % TODO: Mostrar imagen suubliminal
+    % Mostramos la imagen subliminal
+    imsub=imread('test.png', 'png');
+    Screen('PutImage', win, imsub);
+    Screen('Flip', win);
     WaitSecs(wait_is);
     
     Screen('FillRect', win, white); % Limpia retina
-    WaitSecs(wait_p2);
+    Screen('Flip', win);
+    WaitSecs(wait_pl2);
     
-    % TODO: Mostrar imagenes secundarias
+    % Mostramos imagenes secundarias
+    % Cargamos las imagenes
+    imsec1=imread('test.png', 'png');
+    imsec2=imread('test.png', 'png');
+    imsec3=imread('test.png', 'png');
     
+    % Obtenemos el alto mas grande de las tres imagenes
+    max_height=max([size(imsec1,1); size(imsec2,1); size(imsec3,1)]);
+    
+    % Agregamos blanco necesario arriba y abajo a la primera imagen
+    wadd=ones(floor((max_height-size(imsec1,1))/2), size(imsec1,2), 3)*255;
+    padd=ones(mod(max_height-size(imsec1,1),2), size(imsec1,2), 3)*255;
+    imsec1=[wadd; padd; imsec1; wadd];
+    
+    % Agregamos blanco necesario arriba y abajo a la segunda imagen
+    wadd=ones(floor((max_height-size(imsec2,1))/2), size(imsec2,2), 3)*255;
+    padd=ones(mod(max_height-size(imsec2,1),2), size(imsec2,2), 3)*255;
+    imsec2=[wadd; padd; imsec2; wadd];
+    
+    % Agregamos blanco necesario arriba y abajo a la tercera imagen
+    wadd=ones(floor((max_height-size(imsec3,1))/2), size(imsec3,2), 3)*255;
+    padd=ones(mod(max_height-size(imsec3,1),2), size(imsec3,2), 3)*255;
+    imsec3=[wadd; padd; imsec3; wadd];
+    
+    % Las pegamos
+    space=ones(max_height,imgsec_space,3)*255;
+    imsecs=[imsec1 space imsec2 space imsec3];
+    size(imsecs)
+    
+    % Ponemos en pantalla y mostramos
+    Screen('PutImage', win, imsecs);
+    Screen('Flip', win);
+    %TODO: Mensaje: "Presione una tecla cuando este listo para escribir la palabra..."
+    
+    % Esperamos hasta que el usuario toque una tecla
+    pressed = 0;
+    while pressed == 0
+        [pressed, secs, kbData] = KbCheck;  
+    end;
+    %TODO: Como obtengo el tiempo aca?
+    
+    
+    %TODO: Escribir palabra
     
     %Draw i?th image to backbuffer:
     %Screen('DrawTexture', win, textureIndex);
     %Show images exactly 2 refresh cycles apart of each other:
-    [down, secs, keycode]=KbCheck;
-    Screen('FillRect', win, black);
+    %[down, secs, keycode]=KbCheck;
     WaitSecs(0.005);
     %Keyboard checks , whatever ... Next loop iteration .
 end;
