a17a716c0c1523f52bbcd7e93f5b16fa9eb9c4b9
Roberto Rama <bertoski@gmail.com>
2014-09-26 01:32:27 -0300

    Script para obtener las rutas a las imagenes.

---
+++ getImgsPaths.m
@@ -0,0 +1,20 @@
+function [imgs_sub, imgs_sec] = getImgsPaths(base_dir)
+    dirs = dir(base_dir);
+    imgs_sub = cell(size(dirs,1)-2, 1);
+    imgs_sec = cell(size(dirs,1)-2, 3);
+
+    for i=3:size(dirs,1)
+        dir_name = dirs(i).name;
+        files = dir(strcat('imgs\',dir_name));
+        c = 1;
+        for j=3:size(files,1)
+            file_name = files(j).name;
+            if strcmp(file_name(1), 's') && strcmp(file_name(2), '.')
+                imgs_sub{i-2} = strcat('imgs\',dir_name,'\',file_name);
+            else
+                imgs_sec{i-2,c} = strcat('imgs\',dir_name,'\',file_name);
+                c = c + 1;
+            end
+        end
+    end
+end
