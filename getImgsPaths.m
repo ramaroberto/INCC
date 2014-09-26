8e09154e399f20648de905674ea680a1af1a9720
Roberto Rama <bertoski@gmail.com>
2014-09-26 02:15:28 -0300

    Agregadas la obtencion de etiquetas para futuro analisis.

--- getImgsPaths.m
+++ getImgsPaths.m
@@ -1,10 +1,12 @@
-function [imgs_sub, imgs_sec] = getImgsPaths(base_dir)
+function [imgs_sub, imgs_sec, imgs_ids] = getImgsPaths(base_dir)
     dirs = dir(base_dir);
     imgs_sub = cell(size(dirs,1)-2, 1);
     imgs_sec = cell(size(dirs,1)-2, 3);
+    imgs_ids = cell(size(dirs,1)-2, 1);
 
     for i=3:size(dirs,1)
         dir_name = dirs(i).name;
+        imgs_ids{i-2} = dir_name;
         files = dir(strcat('imgs\',dir_name));
         c = 1;
         for j=3:size(files,1)
