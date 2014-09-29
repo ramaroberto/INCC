958f20eb19a0bfb6c65a7c8eee5ac1c22bf5e468
Sabrina <sizcovich@dc.uba.ar>
2014-09-29 16:51:36 -0300

    Agrego mas imagenes de prueba.

--- getImgsPaths.m
+++ getImgsPaths.m
@@ -7,14 +7,14 @@ function [imgs_sub, imgs_sec, imgs_ids] = getImgsPaths(base_dir)
     for i=3:size(dirs,1)
         dir_name = dirs(i).name;
         imgs_ids{i-2} = dir_name;
-        files = dir(strcat('imgs\',dir_name));
+        files = dir(strcat('imgs/',dir_name));
         c = 1;
         for j=3:size(files,1)
             file_name = files(j).name;
             if strcmp(file_name(1), 's') && strcmp(file_name(2), '.')
-                imgs_sub{i-2} = strcat('imgs\',dir_name,'\',file_name);
+                imgs_sub{i-2} = strcat('imgs/',dir_name,'/',file_name);
             else
-                imgs_sec{i-2,c} = strcat('imgs\',dir_name,'\',file_name);
+                imgs_sec{i-2,c} = strcat('imgs/',dir_name,'/',file_name);
                 c = c + 1;
             end
         end
