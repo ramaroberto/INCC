fd61fc2d05122e5394a5b4bb0add2397d56683c8
Roberto Alejandro Rama Vilarino <roberto@enlasramas.com.ar>
2014-10-29 17:59:47 -0300

    Modificacion de grafico

--- graficos/graficoComparativoPorImagenesSinTiempo.m
+++ graficos/graficoComparativoPorImagenesSinTiempo.m
@@ -1,28 +1,19 @@
-promedioSubl = zeros(25,1);
-promedioFruta = zeros(25,1);
-sumaFruta = zeros(25,1);
-sumaSubl = zeros(25,1);
+cuentaCorrectas = zeros(25,1);
+cuentaIncorrectas = zeros(25,1);
 for i=1:length(s)
     resta = ones(length(s{i}),1) - s{i}(:,2);
-    [m n] = size(s{i});
-    for j=1:m
+    for j=1:length(s{i})
         indice = s{i}(j,1) - 1;
-        promedioFruta(indice) = promedioFruta(indice) + s{i}(j,2)*s{i}(j,6);
-        promedioSubl(indice) = promedioSubl(indice) + resta(j)*s{i}(j,5);
-        sumaFruta(indice) = sumaFruta(indice) + s{i}(j,2)*s{i}(j,6);
-        sumaSubl(indice) = sumaSubl(indice) + resta(j)*s{i}(j,5);
+        if s{i}(j,6) == 1 || s{i}(j,5) == 1
+            cuentaCorrectas(indice) = cuentaCorrectas(indice) + 1;
+        else
+            cuentaIncorrectas(indice) = cuentaIncorrectas(indice) + 1;
+        end
     end
 end
-for i=1:25
-    %promedioFruta(i) = promedioFruta(i)/(sumaFruta(i)+0.0000000000001);
-    %promedioSubl(i) = promedioSubl(i)/(sumaSubl(i)+0.0000000000001);
-    %bar(i+(i-1)*2, promedioFruta(i))
-    %hold on
-    %bar(i+(i-1)*2+1,promedioSubl(i))
-    %hold on
-    
+for i=1:25    
     hold on
-    bar(i, promedioSubl(i)-promedioFruta(i))
+    bar(i, cuentaCorrectas(i)-cuentaIncorrectas(i))
 end
 
 set(gca,'xtick', 1:25)
