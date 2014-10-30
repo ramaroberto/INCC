155875b44f98f711ed60e04d4e5f39373d6a80ef
Roberto Rama <bertoski@gmail.com>
2014-10-29 23:35:34 -0300

    Agrego ultimo test de hipotesis.

--- doHTests.m
+++ doHTests.m
@@ -46,7 +46,6 @@ for i=1:length(s)
     [h,p] = ttest(dfr,dsb);
     resHTSuj = [resHTSuj; [i,h,p]];
 end
-
 resHTSuj
 
 % Test sobre el tiempo de cada imagen
@@ -99,5 +98,73 @@ for i=1:25
     resHTImg = [resHTImg; [i,h,p]];
 
 end
-
 resHTImg
+
+% Test total
+
+% Calculo means
+meanTiempoSubl = [];
+meanTiempoFruta = [];
+for i=1:25
+    meanTiempoSubl = [meanTiempoSubl mean(tiempoSubl{i})];
+    meanTiempoFruta = [meanTiempoFruta mean(tiempoFruta{i})];
+end
+
+resTot = [];
+dfr = [];
+dsb = [];
+for i=1:length(s)    
+    for j=1:length(s{i})
+        % Si tenemos un dato acertado y correspondiente a fruta
+        if s{i}(j,6) == 1
+            dfr = [dfr s{i}(j,3)-meanTiempoFruta(j)];
+        end
+        
+        % Si tenemos un dato acertado y correspondiente a subliminal
+        if s{i}(j,5) == 1
+            dsb = [dsb s{i}(j,3)-meanTiempoSubl(j)];
+        end
+    end
+end
+
+mean(dfr)
+mean(dsb)
+
+% Como tengo diferente cantidad de datos para cada vector calculo la
+% media del que tiene mas elementos y saco aquellos valores que tienen
+% una mayor diferencia absoluta con la media.
+if length(dfr) ~= length(dsb)
+    diff = abs(length(dfr)-length(dsb));
+    if length(dfr) > length(dsb)
+        adfr = abs(dfr - mean(dfr) * ones(1, length(dfr)));
+        [~, ind] = sort(adfr);
+
+        to_delete = sort(ind(1:diff), 'descend');
+        for z=1:length(to_delete)
+            dfr(to_delete(z)) = [];
+        end
+    else
+        adsb = abs(dsb - mean(dsb) * ones(1, length(dsb)));
+        [~, ind] = sort(adsb);
+
+        to_delete = sort(ind(1:diff), 'descend');
+        for z=1:length(to_delete)
+            dsb(to_delete(z)) = [];
+        end
+    end
+end
+
+% Realizo el test de hipotesis.
+[h,p] = ttest(dfr,dsb)
+
+
+
+
+
+
+
+
+
+
+
+
